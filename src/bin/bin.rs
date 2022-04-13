b4484e1fda53f725f087736dc8cf7b89d888b1d4
Mathias Panzenböck <grosser.meister.morti@gmx.net>
2022-04-13 04:07:48 +0200

    use clap instead of argparse

--- src/bin/bin.rs
+++ src/bin/bin.rs
@@ -1,27 +1,18 @@
-use argparse::{ArgumentParser, StoreTrue, Collect, Print};
+use clap::Parser;
 use imsz::imsz;
 
 fn main() {
-    struct Options {
-        verbose: bool,
+    #[derive(Parser, Debug)]
+    #[clap(author, version, about, long_about =
+        "The imsz library gets image size from \
+         files, this is a demo application.")]
+    struct Args {
         files: Vec<String>,
     }
 
-    let mut options = Options { verbose: false, files: [].to_vec()};
-    {
-        let mut ap = ArgumentParser::new();
-        ap.set_description("The imsz library gets image size from \
-                            files, this is a demo application.");
-        ap.refer(&mut options.verbose)
-            .add_option(&["-v", "--verbose"], StoreTrue, "Be verbose");
-        ap.add_option(&["-V", "--version"],
-            Print(env!["CARGO_PKG_VERSION"].to_string()), "Display version and exits");
-        ap.refer(&mut options.files)
-            .add_argument("files", Collect, "List of files to process").required();
-        ap.parse_args_or_exit();
-    }
+    let args = Args::parse();
 
-    for fname in options.files.iter() {
+    for fname in &args.files {
         match imsz(fname) {
             Ok(info) => println!("{}: {}, {} x {}", fname, info.format, info.width, info.height),
             Err(error) => eprintln!("{}: {}", fname, error)
